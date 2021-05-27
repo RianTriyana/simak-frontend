4a644a31743af3e3a5e69f954867f640a47f6fdf
rian triyana <riantriyana94@gmail.com>
2021-05-27 19:18:01 +0700

    first upload project

---
+++ frontend/script/script.js
@@ -0,0 +1,12 @@
+
+// sidebar-menu
+
+var header = document.getElementById("sidebar-menu");
+var menus = header.getElementsByClassName("menu");
+for (var i = 0; i < menus.length; i++) {
+  menus[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
